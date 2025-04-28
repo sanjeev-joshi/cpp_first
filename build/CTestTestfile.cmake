102603b177d612f407d4a1bbc6a571bb71af19ae
Mahesha Mutharayappa (SDS/EPE2) <Mutharayappa.Mahesha@in.bosch.com>
2025-04-28 20:36:03 +0530

    removed wrong files
    Signed-off-by: Mahesha Mutharayappa (SDS/EPE2) <Mutharayappa.Mahesha@in.bosch.com>

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /data1/cpp_projects/project01
-# Build directory: /data1/cpp_projects/project01/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(UtilsTests "/data1/cpp_projects/project01/build/run_tests")
-set_tests_properties(UtilsTests PROPERTIES  _BACKTRACE_TRIPLES "/data1/cpp_projects/project01/CMakeLists.txt;31;add_test;/data1/cpp_projects/project01/CMakeLists.txt;0;")
